7d8ca242c1cbd491524fb577bffea1ccd6e9e768
Aryan Dubey <aryandubey@Aryans-MacBook-Air.local>
2024-06-15 14:35:58 +0530

    Evaluation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cnnClassifier import logger
 from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
 from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
+from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
 #Data downloading and ingestion part
 
@@ -18,17 +19,17 @@ from cnnClassifier.pipeline.stage_03_model_training import ModelTrainingPipeline
 
 # #Base model creation
 
-# STAGE_NAME = "Prepare base model"
-# try: 
-#    logger.info(f"*******************")
-#    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
-#    prepare_base_model = PrepareBaseModelTrainingPipeline()
-#    logger.info(f">>>>>> stage inbetween started <<<<<<")
-#    prepare_base_model.main()
-#    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
-# except Exception as e:
-#         logger.exception(e)
-#         raise e
+STAGE_NAME = "Prepare base model"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   prepare_base_model = PrepareBaseModelTrainingPipeline()
+   logger.info(f">>>>>> stage inbetween started <<<<<<")
+   prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
 
 
@@ -45,3 +46,16 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
